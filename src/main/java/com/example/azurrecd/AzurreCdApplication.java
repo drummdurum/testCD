0a54a211fff2c85910a07b3255b3bec7bb6577de
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-09-12 13:11:24 +0200

    jddj

---
+++ src/main/java/com/example/azurrecd/AzurreCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurrecd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurreCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzurreCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azurrecd/AzurreCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurrecd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurreCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
